5e2700eb59b3bccef3ff54b8e750b0a9c73a17e1
Роман Дикарев <super.romanchik228@yandex.ru>
2025-09-14 14:53:12 +0300

    удалил старый апи для получения данных о заказах

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,4 @@ from myapp.views import products_stat_view
 pp_name = 'myapp'  #чтобы файл был действительно отдельным пространством имен
 
 
-urlpatterns = [
-    path("api/products-stat/", products_stat_view, name="products-stat"),
-]
+urlpatterns = []
