f7f2bdc41915be8899ed1eb5d5086b25a251df17
Роман Дикарев <super.romanchik228@yandex.ru>
2025-09-16 15:06:22 +0300

    добавлена API для остатков

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import MetaData, Table, select, create_engine
@@ -9,7 +10,7 @@ from context_logger import ContextLogger
 import logging
 from dateutil.parser import parse as parse_date
 
-logger = ContextLogger(logging.getLogger("fastapi"))
+logger = ContextLogger(logging.getLogger("fastapi_app"))
 
 app = FastAPI()
 
@@ -24,9 +25,10 @@ metadata.reflect(bind=sync_engine)
 products_table = metadata.tables.get("myapp_productsstat")
 nmids_table = metadata.tables.get("myapp_nmids")
 wblk_table = metadata.tables.get("myapp_wblk")
+stocks_table = metadata.tables.get("myapp_stocks")
 
-if None in [products_table, nmids_table, wblk_table]:
-    logger.error("Одна из таблиц (ProductsStat, nmids, WbLk) не найдена.")
+if None in [products_table, nmids_table, wblk_table, stocks_table]:
+    logger.error("Одна из таблиц (ProductsStat, nmids, WbLk, Stocks) не найдена.")
     raise RuntimeError()
 
 
@@ -49,8 +51,18 @@ class ProductsStatRequest(BaseModel):
     colors: list[str] | None = None
 
 
-@app.post("/products_stat/")
+class ProductStatResponse(BaseModel):
+    rub: float
+    sht: int
+
+
+@app.post("/products_stat/", response_model=Dict[int, ProductStatResponse])
 async def products_stat_endpoint(payload: ProductsStatRequest):
+    """
+    Получить информауию о заказах
+    :param payload:
+    :return:
+    """
     try:
         # Парсим даты
         date_from = parse_date(payload.date_from).date() if payload.date_from else None
@@ -73,7 +85,6 @@ async def products_stat_endpoint(payload: ProductsStatRequest):
         if payload.articles:
             query_nmids = query_nmids.where(nmids_table.c.nmid.in_(payload.articles))
 
-        # nmids_list = [row["nmid"] for row in await database.fetch_all(query_nmids)]
         nmids_rows = await database.fetch_all(query_nmids)
 
         nmids_list = []
@@ -130,10 +141,120 @@ async def products_stat_endpoint(payload: ProductsStatRequest):
             sht = i["ordersCount"]
 
             if nmid in all_data:
-                all_data[nmid]["руб"] += rub
-                all_data[nmid]["шт"] += sht
+                all_data[nmid]["rub"] += rub
+                all_data[nmid]["sht"] += sht
             else:
-                all_data[nmid] = {"руб": rub, "шт": sht}
+                all_data[nmid] = {"rub": rub, "sht": sht}
+        return all_data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Pydantic-модель для входящего POST
+class ProductsQuantRequest(BaseModel):
+    inn: int
+    articles: list[int] | None = None
+    colors: list[str] | None = None
+    sizes: list[int] | None = None
+    warhouses: list[str] | None = None
+
+
+class ProductQuantResponse(BaseModel):
+    quantity: float
+    inwaytoclient: int
+    inwayfromclient: int
+
+
+@app.post("/quantity/", response_model=Dict[int, ProductQuantResponse])
+async def products_quantity_endpoint(payload: ProductsQuantRequest):
+    try:
+        # Получаем lk
+        query_lk = select(wblk_table).where(wblk_table.c.inn == payload.inn)
+        lk_row = await database.fetch_one(query_lk)
+        if not lk_row:
+            raise HTTPException(status_code=404, detail="WbLk не найден")
+
+        lk_id = lk_row["id"]
+
+        # Получаем список nmid по lk
+        query_nmids = select(
+            nmids_table.c.nmid,
+            nmids_table.c.characteristics
+        ).where(nmids_table.c.lk_id == lk_id)
+
+        if payload.articles:
+            query_nmids = query_nmids.where(nmids_table.c.nmid.in_(payload.articles))
+
+        nmids_rows = await database.fetch_all(query_nmids)
+
+        nmids_list = []
+
+        for row in nmids_rows:
+            nmid = row["nmid"]
+            if payload.colors:
+                characteristics = row["characteristics"]  # JSONField из Django
+                if characteristics is None:
+                    continue
+
+                try:
+                    parsed = (
+                        characteristics
+                        if isinstance(characteristics, list)
+                        else json.loads(characteristics)
+                    )
+                except Exception:
+                    continue
+
+                color_entry = next(
+                    (item for item in parsed if item.get("id") == 14177449), None
+                )
+                if not color_entry:
+                    continue
+
+                value = color_entry.get("value")
+                if not value or not isinstance(value, list):
+                    continue
+
+                color_value = value[0].lower()  # берём первое значение
+                if color_value in [c.lower() for c in payload.colors]:
+                    nmids_list.append(nmid)
+            else:
+                nmids_list.append(nmid)
+
+        if not nmids_list:
+            return []
+
+        query_stocks = select(stocks_table).where(
+            stocks_table.c.nmid.in_(nmids_list)
+        )
+
+        if payload.sizes:
+            query_stocks = query_stocks.where(stocks_table.c.techsize.in_(payload.sizes))
+
+        if payload.warhouses:
+            query_stocks = query_stocks.where(stocks_table.c.warehousename.in_(payload.warhouses))
+
+        stock_rows = await database.fetch_all(query_stocks)
+        row_data = [dict(row._mapping) for row in stock_rows]
+        # return row_data
+
+        all_data = {}
+        for i in row_data:
+            nmid = i["nmid"]
+            inwaytoclient = i["inwaytoclient"]
+            inwayfromclient = i["inwayfromclient"]
+            quantity = i["quantity"]
+
+            if nmid in all_data:
+                all_data[nmid]["quantity"] += quantity
+                all_data[nmid]["inwaytoclient"] += inwaytoclient
+                all_data[nmid]["inwayfromclient"] += inwayfromclient
+            else:
+                all_data[nmid] = {}
+                all_data[nmid]["quantity"] = quantity
+                all_data[nmid]["inwaytoclient"] = inwaytoclient
+                all_data[nmid]["inwayfromclient"] = inwayfromclient
         return all_data
 
     except Exception as e:
