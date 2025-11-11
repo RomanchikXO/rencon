f3ac5c74089fd1100e06e2a47e957d268317b366
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-11 14:19:09 +0300

    - дебагинг тестового таска - добавил декоратор подколючения к бд в фаст апи

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 from log_context import task_context
 from functools import wraps
+from fastapi_app.main import database
 
 
 def with_task_context(task_name):
@@ -13,3 +14,23 @@ def with_task_context(task_name):
                 task_context.reset(token)
         return wrapper
     return decorator
+
+
+def with_db_connection(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        connected_here = False
+        try:
+            # Подключаемся, если база не активна
+            if not database.is_connected:
+                await database.connect()
+                connected_here = True
+
+            return await func(*args, **kwargs)
+
+        finally:
+            # Закрываем соединение, если именно этот вызов его открыл
+            if connected_here and database.is_connected:
+                await database.disconnect()
+
+    return wrapper
--- tasks/drop_to_goo_table.py
+++ tasks/drop_to_goo_table.py
@@ -2,23 +2,11 @@ from fastapi_app.main import get_dimensions, database
 from loader import BEARER
 from context_logger import ContextLogger
 import logging
+from decorators import with_db_connection
 
 logger = ContextLogger(logging.getLogger("core"))
 
+@with_db_connection
 async def do_something():
-    try:
-        # Подключаемся к базе вручную
-        if not database.is_connected:
-            await database.connect()
-
-        # Вызываем эндпоинт напрямую
-        result = await get_dimensions(token=BEARER)
-        logger.info(result)
-
-    except Exception as e:
-        logger.error(f"Ошибка при выполнении get_dimensions: {e}")
-
-    finally:
-        # Закрываем соединение, чтобы не держать лишние подключения
-        if database.is_connected:
-            await database.disconnect()
+    result = await get_dimensions(token=BEARER)
+    logger.info(result)
