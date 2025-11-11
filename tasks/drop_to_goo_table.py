a386ba3ea314c239ab03d4129d81dfb2b89fe436
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-11 14:02:18 +0300

    - добавил логи в тестовый таск

--- tasks/drop_to_goo_table.py
+++ tasks/drop_to_goo_table.py
@@ -1,15 +1,17 @@
-from fastapi_app.main import get_dimensions
+from fastapi.testclient import TestClient
+from fastapi_app.main import app
 from loader import BEARER
 from context_logger import ContextLogger
 import logging
 
+client = TestClient(app)
 
 logger = ContextLogger(logging.getLogger("core"))
 
 async def do_something():
     try:
-        result = await get_dimensions(token=BEARER)
+        response = client.get("/dimensions/", headers={"Authorization": BEARER})
     except Exception as e:
         logger.error(e)
         raise
-    logger.info(result)
+    logger.info(response)
