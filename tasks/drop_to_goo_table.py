0563127afdecab35b98ebf5f88e2ff46bac60ea5
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-11 14:16:01 +0300

    - дебагинг тестового таска

--- tasks/drop_to_goo_table.py
+++ tasks/drop_to_goo_table.py
@@ -1,4 +1,4 @@
-from fastapi_app.main import get_dimensions
+from fastapi_app.main import get_dimensions, database
 from loader import BEARER
 from context_logger import ContextLogger
 import logging
@@ -7,7 +7,18 @@ logger = ContextLogger(logging.getLogger("core"))
 
 async def do_something():
     try:
+        # Подключаемся к базе вручную
+        if not database.is_connected:
+            await database.connect()
+
+        # Вызываем эндпоинт напрямую
         result = await get_dimensions(token=BEARER)
         logger.info(result)
+
     except Exception as e:
         logger.error(f"Ошибка при выполнении get_dimensions: {e}")
+
+    finally:
+        # Закрываем соединение, чтобы не держать лишние подключения
+        if database.is_connected:
+            await database.disconnect()
