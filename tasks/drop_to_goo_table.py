f118931dc301994e2688f68500828d0435cf1d00
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-11 14:08:15 +0300

    - дебагинг тестового таска

--- tasks/drop_to_goo_table.py
+++ tasks/drop_to_goo_table.py
@@ -8,10 +8,10 @@ client = TestClient(app)
 
 logger = ContextLogger(logging.getLogger("core"))
 
-async def do_something():
+def do_something():
     try:
         response = client.get("/dimensions/", headers={"Authorization": f"Bearer {BEARER}"})
+        logger.info(response.json())  # выведем JSON-ответ
     except Exception as e:
         logger.error(e)
         raise
-    logger.info(response)
