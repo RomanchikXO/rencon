e20725a68ccba0c323c5f52b368871d3c65b2f61
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-21 18:14:22 +0300

    - дебаг

--- django_app/celery_config.py
+++ django_app/celery_config.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import os
+from django.utils import timezone
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
@@ -7,6 +8,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 app = Celery('rencon')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.now = timezone.now
 
 
 # Использование DatabaseScheduler для хранения расписания в базе данных
