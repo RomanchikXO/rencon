17d1e2a81372d3f7a6ec79ba3bf741c52db07d05
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-24 17:18:43 +0300

    - дебаг

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class tg_user(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="tg_profile")
