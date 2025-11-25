7c49434d20093100f5c762fcadea57cb7c1f69dd
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-25 16:49:38 +0300

    - добавил тестовый таск

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -3,7 +3,7 @@ import asyncio
 
 from parsers.wildberies import (get_nmids, get_stocks_data_2_weeks, get_stock_age_by_period,
                                 get_stat_products, get_advs, get_advs_stat, get_fin_report,
-                                make_and_get_save_report, get_region_sales)
+                                make_and_get_save_report, get_region_sales, get_time_str)
 from parsers.my_sklad import get_and_save_mysklad_data, update_google_table_mysklad
 from tasks.drop_to_goo_table import (upload_dimensions_to_google, upload_advconversion_to_google,
                                      upload_advcost_to_google, upload_salesreport_to_google, upload_ostatki_to_google,
@@ -15,6 +15,15 @@ from decorators import with_task_context
 from context_logger import ContextLogger
 
 logger = ContextLogger(logging.getLogger("myapp"))
+
+
+@shared_task
+@with_task_context("get_time_str")
+def get_time_str_task():
+    logger.info("🟢 Тест задача")
+    a = asyncio.run(get_time_str(format="%d.%m.%y"))
+
+    logger.info(f"Конец задачи {a}")
 
 
 @shared_task
